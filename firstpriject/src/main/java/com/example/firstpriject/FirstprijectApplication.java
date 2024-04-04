85ddd76263d83ded951aee92994e9f672be4342e
changkiyun <ekzm7845@naver.com>
2024-04-04 16:02:46 +0900

    First Commit

---
+++ firstpriject/src/main/java/com/example/firstpriject/FirstprijectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstpriject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprijectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprijectApplication.class, args);
+	}
+
+}
---
+++ firstpriject/src/test/java/com/example/firstpriject/FirstprijectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstpriject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprijectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
