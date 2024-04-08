69419bebbb9f788efa220a0bad74366c79dd7f95
changkiyun <ekzm7845@naver.com>
2024-04-08 17:27:23 +0900

    Chapter8 Member Delete

--- firstpriject/src/main/java/com/example/firstpriject/controller/MemberController.java
+++ firstpriject/src/main/java/com/example/firstpriject/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -76,6 +77,18 @@ public class MemberController {
             memberRepository.save(memberEntity);
         }
         return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        Member target = memberRepository.findById(id).orElse(null);
+
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다!!");
+        }
+
+        return "redirect:/members";
     }
 
     @PostConstruct
